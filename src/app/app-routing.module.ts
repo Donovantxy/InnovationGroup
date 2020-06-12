3e219722993b5b539fcbd34d43e3cdcc7920c789
Fulvio <donovantxy@gmail.com>
2020-06-12 17:55:18 +0100

    initialised components, server and asset

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductComponent } from './components/product/product.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ProductListComponent,
+  },
+  {
+    path: 'product/:id',
+    component: ProductComponent,
+  },
+  {path: '**', redirectTo: ''},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
