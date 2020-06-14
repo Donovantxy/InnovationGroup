fa57712956ec63223e2d4ee0559827b16e90df50
Fulvio <donovantxy@gmail.com>
2020-06-14 20:04:39 +0100

    retrieve data from a constant within the project

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,10 +1,11 @@
 import { Product } from 'src/app/store/state/basket.state';
-import { map, catchError } from 'rxjs/operators';
+import { map, catchError, tap } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of, Subject, BehaviorSubject } from 'rxjs';
+import { Observable, of, BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Router } from '@angular/router';
+import { fruis } from '../store/data';
 
 interface ResponseResource {
   product: Product;
@@ -25,18 +26,27 @@ export class ProductsService {
   ) { }
 
   getProducts(): Observable<any> {
-    return this.http.get(environment.api.products)
-    .pipe(map((prods: Product[]) => {
-      this.products = prods;
-      prods.map(p => p.img = `id-${p.id}.jpg`);
-      this.sortProducts('name');
-      return prods;
-    }));
+    // return this.http.get(environment.api.products)
+    if (!this.products) {
+      return of(fruis)
+      .pipe(map((prods: Product[]) => {
+        this.products = prods;
+        prods.map(p => p.img = `id-${p.id}.jpg`);
+        this.sortProducts('name');
+        return prods;
+      }));
+    }
+    return of(this.products);
   }
 
   getProduct(id: number|string): Observable<any> {
-    return this.http.get(`${environment.api.products}/${id}`)
+    // return this.http.get(`${environment.api.products}/${id}`)
+    return of({
+      product: fruis.find(fr => fr.id === +id),
+      success: true,
+    })
     .pipe(
+      tap(console.log),
       map((resp: ResponseResource) => {
         if (resp.success) {
           return {...resp.product, img: `id-${resp.product.id}.jpg`};
---
+++ src/app/store/data.ts
@@ -0,0 +1,34 @@
+import { Product } from 'src/app/store/state/basket.state';
+
+export const fruis: Product[] = [
+  {
+    id: 0,
+    name: 'Apple',
+    description: 'An apple is an edible fruit produced by an apple tree (Malus domestica). Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today. Apples have been grown for thousands of years in Asia and Europe and were brought to North America by European colonists. Apples have religious and mythological significance in many cultures, including Norse, Greek and European Christian tradition.<br>Apple trees are large if grown from seed. Generally, apple cultivars are propagated by grafting onto rootstocks, which control the size of the resulting tree. There are more than 7,500 known cultivars of apples, resulting in a range of desired characteristics. Different cultivars are bred for various tastes and use, including cooking, eating raw and cider production. Trees and fruit are prone to a number of fungal, bacterial and pest problems, which can be controlled by a number of organic and non-organic means. In 2010, the fruit\'s genome was sequenced as part of research on disease control and selective breeding in apple production.<br>Worldwide production of apples in 2018 was 86 million tonnes, with China accounting for nearly half of the total.',
+    price: 0.75
+  },
+  {
+    id: 1,
+    name: 'Banana',
+    description: 'A banana is an elongated, edible fruit – botanically a berry – produced by several kinds of large herbaceous flowering plants in the genus Musa. In some countries, bananas used for cooking may be called \"plantains\", distinguishing them from dessert bananas. The fruit is variable in size, color, and firmness, but is usually elongated and curved, with soft flesh rich in starch covered with a rind, which may be green, yellow, red, purple, or brown when ripe. The fruits grow in clusters hanging from the top of the plant. Almost all modern edible seedless (parthenocarp) bananas come from two wild species – Musa acuminata and Musa balbisiana. The scientific names of most cultivated bananas are Musa acuminata, Musa balbisiana, and Musa × paradisiaca for the hybrid Musa acuminata × M. balbisiana, depending on their genomic constitution. The old scientific name for this hybrid, Musa sapientum, is no longer used.<br>Musa species are native to tropical Indomalaya and Australia, and are likely to have been first domesticated in Papua New Guinea. They are grown in 135 countries, primarily for their fruit, and to a lesser extent to make fiber, banana wine, and banana beer and as ornamental plants. The world\'s largest producers of bananas in 2017 were India and China, which together accounted for approximately 38% of total production.',
+    price: 1.05
+  },
+  {
+    id: 2,
+    name: 'Orange',
+    description: 'The orange is the fruit of various citrus species in the family Rutaceae (see list of plants known as orange); it primarily refers to Citrus × sinensis, which is also called sweet orange, to distinguish it from the related Citrus × aurantium, referred to as bitter orange. The sweet orange reproduces asexually (apomixis through nucellar embryony); varieties of sweet orange arise through mutations.<br>The orange is a hybrid between pomelo (Citrus maxima) and mandarin (Citrus reticulata). The chloroplast genome, and therefore the maternal line, is that of pomelo. The sweet orange has had its full genome sequenced.<br>The orange originated in a region comprising Southern China, Northeast India, and Myanmar, and the earliest mention of the sweet orange was in Chinese literature in 314 BC. As of 1987, orange trees were found to be the most cultivated fruit tree in the world. Orange trees are widely grown in tropical and subtropical climates for their sweet fruit. The fruit of the orange tree can be eaten fresh, or processed for its juice or fragrant peel. As of 2012, sweet oranges accounted for approximately 70% of citrus production.<br>In 2017, 73 million tonnes of oranges were grown worldwide, with Brazil producing 24% of the world total, followed by China and India.',
+    price: 0.50
+  },
+  {
+    id: 3,
+    name: 'Cherry',
+    description: 'A cherry is the fruit of many plants of the genus Prunus, and is a fleshy drupe (stone fruit).<br>Commercial cherries are obtained from cultivars of several species, such as the sweet Prunus avium and the sour Prunus cerasus. The name \'cherry\' also refers to the cherry tree and its wood, and is sometimes applied to almonds and visually similar flowering trees in the genus Prunus, as in \"ornamental cherry\" or \"cherry blossom\". Wild cherry may refer to any of the cherry species growing outside cultivation, although Prunus avium is often referred to specifically by the name \"wild cherry\" in the British Isles.',
+    price: 2.50
+  },
+  {
+    id: 4,
+    name: 'Pineapple',
+    description: 'The pineapple (Ananas comosus) is a tropical plant with an edible fruit and the most economically significant plant in the family Bromeliaceae. The pineapple is indigenous to South America, where it has been cultivated for many centuries. The introduction of the pineapple cultivation to Europe in the 17th century made it a significant cultural icon of luxury. Since the 1820s, pineapple has been commercially grown in greenhouses and many tropical plantations. Further, it is the third most important tropical fruit in world production. In the 20th century, Hawaii was a dominant producer of pineapples, especially for the US; however, by 2016, Costa Rica, Brazil, and the Philippines accounted for nearly one-third of the world\'s production of pineapples.<br>Pineapples grow as a small shrub; the individual flowers of the unpollinated plant fuse to form a multiple fruit. The plant is normally propagated from the offset produced at the top of the fruit, or from side shoot, and typically mature within a year.',
+    price: 1.15
+  }
+];
